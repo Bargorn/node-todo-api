7c87d1480f8b06f318385fe899dc5ffa5b8081fc
Bargorn <fabianogosiltel@gmail.com>
2017-09-13 19:30:42 -0300

    adding hash to password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+// var data = {
+//   id: 10
+// };
+//
+//
+// var token = jwt.sign(data, 'abc123');
+// console.log(token);
+// var decoded = jwt.verify(token, 'abc123');
+// console.log(decoded);
 
+var password = '123abc!';
 
-var token = jwt.sign(data, 'abc123');
-console.log(token);
-var decoded = jwt.verify(token, 'abc123');
-console.log(decoded);
+bcrypt.genSalt(10, (err, salt)=>{
+  bcrypt.hash(password, salt, (err, hash)=>{
+    console.log(hash)
+  });
+});
+
+var hashedPassword = '$2a$10$kZ9aWm20tmSj2OP9xA54.eWc64d0yGzWu/./Ymx3ALb1bsSYaPu0e';
+
+bcrypt.compare(password, hashedPassword, (err, result)=>{
+  console.log(result);
+});
